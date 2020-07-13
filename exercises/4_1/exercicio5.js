422fe7c709596a2dc419bb492aa4786ee69a2faf
Alana Monteiro <lanaah.15@gmail.com>
2020-07-13 18:40:13 -0300

    exercicio 5 boolean

---
+++ exercises/4_1/exercicio5.js
@@ -0,0 +1,9 @@
+let a = 45;
+let b = 40;
+let c = 95;
+
+if (a + b + c != 180){
+    console.log("Erro: O triângulo deve possuir a somatória dos ângulos igual a 180.")
+} else {
+    return (a === 90 || b === 90 || c == 90);
+}
